81520c1867eea1ab3b8c70e2e4b2f93b96e93678
Musharraf-712 <musharrafsiddiqui712@gmail.com>
2025-11-11 12:03:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+const divs = document.querySelectorAll('#parent div');
+let dragged = null;
+
+divs.forEach((div) =>{
+	div.addEventListener('dragstsrt', (e) => {
+		dragged = e.target;
+		e.target.classList.add('selected');
+	})
+
+	div.addEventListener('dragend',(e) => {
+		e.target.classList.remove('selected');
+	})
+	
+	div.addEventListener('dragover',(delt)=>{
+		delt.preventDefault();
+	})
+
+	div.addEventListener('drop',(delt)=>{
+		delt.preventDefault();
+		if(dragged === delt.target) return;
+
+		const temp = dragged.style.bckgroundImage;
+		dragged.style.backgroundImage = delt.target.style.backgroundImage;
+		delt.target.style.backgroundImage = temp
+	})
+})
